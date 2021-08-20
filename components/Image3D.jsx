eed294a473246bbe68a2f5d03435c2bbf4fc688a
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 22:55:53 +0800

    resized img

--- components/Image3D.jsx
+++ components/Image3D.jsx
@@ -1,10 +1,29 @@
+import PropTypes from "prop-types";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const imgVariants = {
+  hidden: {
+    scale: 0.7,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      mass: 1,
+      damping: 15,
+      delay: 1,
+    },
+  },
+};
 
 export default function Image3D() {
   return (
     <ImageWrapper>
       <Image
-        src="/img/saly.png"
+        src="/img/saly-compressed.png"
         width={450}
         height={450}
         className="w-80 h-80 object-center object-cover rounded"
@@ -16,9 +35,18 @@ export default function Image3D() {
 
 function ImageWrapper({ children }) {
   return (
-    <div className="mb-10 lg:mb-0 flex justify-center lg:flex-none">
+    <motion.div
+      variants={imgVariants}
+      initial="hidden"
+      animate="visible"
+      className="mb-10 lg:mb-0 flex justify-center lg:flex-none"
+    >
       {" "}
       {children}
-    </div>
+    </motion.div>
   );
 }
+
+ImageWrapper.propTypes = {
+  children: PropTypes.element,
+};
