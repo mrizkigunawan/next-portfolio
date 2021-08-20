a22877d2d40ac0fba33cd8718cbb72fc252a941d
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 23:05:20 +0800

    modified page structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,44 @@
-import Head from "next/head";
-import Nav from "../components/Nav";
+import PropTypes from "prop-types";
+import Container from "../components/Container";
 import Title from "../components/Title";
 import Description from "../components/Description";
 import PortfolioButton from "../components/PortfolioButton";
 import Image3D from "../components/Image3D";
 import GradientBg from "../components/GradientBg";
+import Wrapper from "../components/Layout";
+import ContentWrapper from "../components/ContentWrapper";
+import GradientBg3 from "../components/GradientBg3";
 
 export default function Home() {
   return (
-    <div className="relative font-poppin bg-white">
-      <Head>
-        <title>Gunawan</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Nav />
+    <Wrapper>
+      <Container>
+        <FlexWrapper>
+          <ContentWrapper>
+            <Title />
+            <Description />
+            <PortfolioButton />
+          </ContentWrapper>
 
-      <main className="relative z-10">
-        <div className="max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto py-16">
-          <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between">
-            {/* text */}
-            <div className="text-center lg:text-left space-y-8">
-              <Title />
-              <Description />
-              <div>
-                <PortfolioButton />
-              </div>
-            </div>
-
-            {/* image */}
-            <Image3D />
-          </div>
-        </div>
-      </main>
+          {/* image */}
+          <Image3D />
+        </FlexWrapper>
+      </Container>
 
       <GradientBg />
+      <GradientBg3 />
+    </Wrapper>
+  );
+}
+
+function FlexWrapper({ children }) {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between">
+      {children}
     </div>
   );
 }
+
+FlexWrapper.propTypes = {
+  children: PropTypes.node,
+};
--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,120 +1,180 @@
-import Head from "next/head";
-// import Image from "next/image";
-import Nav from "../components/Nav";
+import Wrapper from "../components/Layout";
+import GradientBg from "../components/GradientBg";
+import Container from "../components/Container";
+import PortfolioDescription from "../components/PortfolioDescription";
+import PortfolioGridWrapper from "../components/PortfolioGridWrapper";
+import PortfolioImage from "../components/PortfolioImage";
+import PortfolioSection from "../components/PortfolioSection";
+import PortfolioTitle from "../components/PortfolioTitle";
+import PortfolioWrapper from "../components/PortfolioWrapper";
+import GradientBg2 from "../components/GradientBg2";
+import GradientBg3 from "../components/GradientBg3";
+import {
+  IconBootstrap,
+  IconCSS,
+  IconDribbble,
+  IconFigma,
+  IconFirebase,
+  IconGithub,
+  IconHTML,
+  IconJS,
+  IconLinkedIn,
+  IconNodeJS,
+  IconReact,
+  IconTailwind,
+  IconWrapper,
+} from "../components/Icons";
 
 export default function Portfolio() {
   return (
-    <div className="relative font-poppin bg-white">
-      <Head>
-        <title>Gunawan | Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Nav />
+    <Wrapper>
+      <div className="relative z-10">
+        <Container className="space-y-12">
+          {/* section desain konten */}
+          <PortfolioSection>
+            <PortfolioWrapper>
+              <PortfolioTitle>Desain Konten</PortfolioTitle>
+              <PortfolioDescription>
+                <p>
+                  Karya desain dari freelance sebagai social media content
+                  designer dan dari organisasi semasa kuliah. Total lebih dari
+                  50 desain telah saya hasilkan.
+                </p>
+              </PortfolioDescription>
+            </PortfolioWrapper>
 
-      <main className="relative z-10">
-        <div className="max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto py-16 space-y-16">
-          <section className="py-12 space-y-6 lg:space-y-10">
-            <div className="lg:flex lg:justify-between lg:items-center space-y-6 lg:space-y-0">
-              <h2 className="text-center lg:text-left text-2xl md:text-3xl xl:text-4xl font-bold text-gray-800">
-                Desain Konten
-              </h2>
-              <div className="lg:max-w-xl text-base lg:text-lg justify-self-end text-gray-600 leading-9 text-center lg:text-left">
-                Karya desain dari freelance sebagai social media content
-                designer dan dari organisasi semasa kuliah.
-              </div>
-            </div>
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-3">
-              {numbers.map((num) => (
-                <img
+            <PortfolioGridWrapper xs={2} md={4}>
+              {[1, 3, 5, 7].map((num) => (
+                <PortfolioImage
                   src={`/img/konten-${num}-compressed.webp`}
                   alt="desain konten instagram"
-                  className="object-cover object-center rounded transform hover:scale-95 hover:rounded-lg transition duration-150 ease-out"
                   key={num}
                 />
               ))}
-            </div>
-          </section>
+            </PortfolioGridWrapper>
+          </PortfolioSection>
 
-          <section className="py-12 space-y-6 lg:space-y-10">
-            <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:items-center space-y-6 lg:space-y-0">
-              <h2 className="text-center lg:text-right text-2xl md:text-3xl xl:text-4xl font-bold text-gray-800">
-                Desain User Interface
-              </h2>
-              <div className="lg:max-w-xl text-base lg:text-lg justify-self-end text-gray-600 leading-9 text-center lg:text-left">
-                Eksplorasi desain user interface dashboard system dan online
-                store webview. Hasil eksplorasi desain tersebut dibuat di Figma.
-              </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+          {/* section desain user interface */}
+          <PortfolioSection>
+            <PortfolioWrapper reverse>
+              <PortfolioTitle right>Desain User Interface</PortfolioTitle>
+              <PortfolioDescription>
+                <p>
+                  Eksplorasi desain user interface dashboard system dan online
+                  store webview. Hasil eksplorasi desain tersebut dibuat di
+                  Figma.
+                </p>
+                <IconWrapper>
+                  <IconFigma />
+                </IconWrapper>
+              </PortfolioDescription>
+            </PortfolioWrapper>
+
+            <PortfolioGridWrapper md={3}>
               {[1, 2, 3].map((num) => (
-                <img
+                <PortfolioImage
                   src={`/img/ui-${num}-compressed.webp`}
-                  alt="desain poster"
-                  className="object-cover object-center rounded transform hover:scale-95 hover:rounded-lg transition duration-150 ease-out"
+                  alt="desain user interface"
                   key={num}
                 />
               ))}
-            </div>
-          </section>
+            </PortfolioGridWrapper>
+          </PortfolioSection>
 
-          <section className="py-12 space-y-6 lg:space-y-10">
-            <div className="lg:flex lg:justify-between lg:items-center space-y-6 lg:space-y-0">
-              <h2 className="text-center lg:text-left text-2xl md:text-3xl xl:text-4xl font-bold text-gray-800">
-                Desain Landing Page
-              </h2>
-              <div className="lg:max-w-xl text-base lg:text-lg justify-self-end text-gray-600 leading-9 text-center lg:text-left">
-                Tampilan responsive landing page website yang dibuat menggunakan
-                HTML, CSS, JavaScript dan Framework Tailwind CSS.
-              </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+          {/* section desain landing page */}
+          <PortfolioSection>
+            <PortfolioWrapper>
+              <PortfolioTitle>Desain Landing Page</PortfolioTitle>
+              <PortfolioDescription>
+                <p>
+                  Tampilan responsive landing page website yang dibuat
+                  menggunakan HTML, CSS, JavaScript dan Framework Tailwind CSS.
+                </p>
+                <IconWrapper>
+                  <IconHTML />
+                  <IconCSS />
+                  <IconJS />
+                  <IconTailwind />
+                </IconWrapper>
+              </PortfolioDescription>
+            </PortfolioWrapper>
+
+            <PortfolioGridWrapper md={2}>
               {[2, 3, 4, 5].map((num) => (
-                <img
+                <PortfolioImage
                   src={`/img/landing-${num}-compressed.webp`}
                   alt="landing page"
-                  className="object-cover object-center rounded transform hover:scale-95 hover:rounded-lg transition duration-150 ease-out"
                   key={num}
                 />
               ))}
-            </div>
-          </section>
+            </PortfolioGridWrapper>
+          </PortfolioSection>
 
-          <section className="py-12 space-y-6 lg:space-y-10">
-            <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:items-center space-y-6 lg:space-y-0">
-              <h2 className="text-center lg:text-right text-2xl md:text-3xl xl:text-4xl font-bold text-gray-800">
-                Projects
-              </h2>
-              <div className="lg:max-w-xl text-base lg:text-lg justify-self-end text-gray-600 leading-9 text-center lg:text-left">
-                Hasil project dari workshop menghasilkan sistem informasi
-                monitoring berbasis website menggunakan teknologi React.js,
-                Google Firebase, dan Cloud Firestor, dan project dari Skripsi
-                Tugas Akhir menghasilkan sistem penyewaan menggunakan React.js,
-                Node.js, dan Cloud Firestore.
-              </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+          {/* section projects */}
+          <PortfolioSection>
+            <PortfolioWrapper reverse>
+              <PortfolioTitle right>Projects</PortfolioTitle>
+
+              <PortfolioDescription>
+                <p>
+                  Projek-projek website dari Workshop dan Tugas Akhir yang
+                  menggunakan teknologi JavaScript, React.js, Bootstrap Admin
+                  Template, MongoDB, Google Firebase, dan Cloud Firestore.
+                </p>
+                <IconWrapper>
+                  <IconReact />
+                  <IconNodeJS />
+                  <IconBootstrap />
+                  <IconFirebase />
+                </IconWrapper>
+              </PortfolioDescription>
+            </PortfolioWrapper>
+
+            <PortfolioGridWrapper md={2}>
               {[2, 3].map((num) => (
-                <img
+                <PortfolioImage
                   src={`/img/project-${num}-compressed.webp`}
                   alt="project"
-                  className="object-cover object-center rounded transform hover:scale-95 hover:rounded-lg transition duration-150 ease-out"
                   key={num}
                 />
               ))}
-            </div>
-          </section>
-        </div>
-      </main>
+            </PortfolioGridWrapper>
+          </PortfolioSection>
+        </Container>
 
-      <div className="mx-auto absolute inset-0 overflow-hidden">
-        <img
-          src="/img/Gradient.png"
-          alt="gradient"
-          className="hidden w-[900px] h-[900px] lg:block transfrom -translate-x-1/3 -translate-y-48 opacity-25 rotate-180"
-        />
+        <div className="relative z-10 px-6 lg:px-0 py-12 text-center">
+          <h3 className="text-xl md:text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-pink-600">
+            Ingin menciptakan sesuatu yang memiliki visual yang indah?
+          </h3>
+          <div className="text-gray-400 mt-12 mb-5">Temukan saya di</div>
+          <div className="flex items-center justify-center space-x-5 lg:space-x-10 my-5">
+            <a
+              href="https://github.com/mrizkigunawan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IconGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rizki-gunawan-609a29150/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IconLinkedIn />
+            </a>
+            <a
+              href="https://dribbble.com/rizkiguna"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IconDribbble />
+            </a>
+          </div>
+        </div>
+        <GradientBg2 />
       </div>
-    </div>
+      <GradientBg />
+      <GradientBg3 />
+    </Wrapper>
   );
 }
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
