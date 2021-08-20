2cd397937ffd6486f6f402216e54eeeabe1f97e6
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 22:50:13 +0800

    add Container.js (page container)

---
+++ components/Container.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+export default function Container({ children, className }) {
+  return (
+    <div
+      className={`relative z-10 px-6 sm:px-0 sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto py-16 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+Container.defaultProps = {
+  className: "",
+};
+
+Container.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
