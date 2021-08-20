8d97922ebfbe855cd1069db8245ec7a30aab2a45
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 22:52:59 +0800

    add `motion` to parent element

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
+import { childVariants } from "./variants.js";
+
 export default function Description() {
   return (
-    <p className="mx-auto lg:mx-0 max-w-lg text-lg text-gray-600 leading-9">
+    <motion.p
+      variants={childVariants}
+      className="mx-auto lg:mx-0 max-w-lg text-lg text-gray-600 dark:text-gray-200 leading-9"
+    >
       Saya suka membangun tampilan website menggunakan teknologi-teknologi
       terkini yang memiliki visual yang baik, tampilan yang interaktif dan
       performance yang cepat. Mengubah ide menjadi sebuah desain adalah hal yang
       saya gemari, lihat lebih lanjut pada portofolio saya.
-    </p>
+    </motion.p>
   );
 }
