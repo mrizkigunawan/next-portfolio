1508ad8cac7e12b03ae5c7f66acdc49a43cc3213
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 22:58:24 +0800

    fix cursor bug

--- components/PortfolioButton.jsx
+++ components/PortfolioButton.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { childVariants } from "./variants.js";
 
 export default function PortfolioButton() {
   return (
-    <Link href="/portfolio">
-      <a className="px-8 py-4 inline-block bg-gray-800 text-gray-50 text-lg shadow-xl hover:bg-gray-700 transition duration-200 ease-out">
-        Lihat Portofolio <span className="ml-2">&rarr;</span>
-      </a>
-    </Link>
+    <div>
+      <Link href="/portfolio">
+        <motion.a
+          variants={childVariants}
+          className="px-8 py-4 inline-block bg-gray-800 dark:bg-gray-200 text-gray-50 dark:text-gray-800 text-lg shadow-xl hover:bg-gray-700 dark:hover:bg-gray-100 transition duration-200 ease-out cursor-pointer"
+        >
+          Lihat Portofolio <span className="ml-2">&rarr;</span>
+        </motion.a>
+      </Link>
+    </div>
   );
 }
