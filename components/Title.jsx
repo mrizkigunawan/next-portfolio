0db18d58f7deb7b35f8697dbdca2ee7c0247ef16
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 23:02:48 +0800

    modified title gradient

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,6 +1,12 @@
+import { motion } from "framer-motion";
+import { childVariants } from "./variants.js";
+
 export default function Title() {
   return (
-    <h1 className="text-3xl md:text-4xl xl:text-5xl font-extrabold text-gray-800 leading-normal lg:space-y-3">
+    <motion.h1
+      variants={childVariants}
+      className="text-3xl md:text-4xl xl:text-5xl font-extrabold text-gray-800 dark:text-gray-50 leading-normal lg:space-y-3"
+    >
       <span className="inline-block md:block">Halo! Saya Gunawan</span>
       <span className="inline-block md:block">
         Seorang{" "}
@@ -9,9 +15,9 @@ export default function Title() {
         </span>{" "}
         dan
       </span>
-      <span className="inline-block md:block text-transparent bg-clip-text bg-gradient-to-r from-purple-800 to-indigo-800">
+      <span className="inline-block md:block text-transparent bg-clip-text bg-gradient-to-r from-purple-800 to-pink-800">
         Developer
       </span>
-    </h1>
+    </motion.h1>
   );
 }
