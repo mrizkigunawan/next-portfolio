21b4a05372e008414ff59aee2ff481fdf971f33b
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-20 23:04:58 +0800

    modified site structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from "next/head";
+import Nav from "../components/Nav";
+import { ThemeWrapper } from "../context/AppContext";
 import "../styles/style.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeWrapper>
+      <Head>
+        <title>Gun Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content="gunawan" />
+        <meta name="description" content="rizki gunawan portfolio" />
+      </Head>
+      <Nav />
+      <Component {...pageProps} />
+    </ThemeWrapper>
+  );
 }
 
 export default MyApp;
