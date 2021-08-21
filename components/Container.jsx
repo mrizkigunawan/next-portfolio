34fad22dd8b34564f1866260ecb1dfef7bd0af30
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-21 12:54:27 +0800

    change maximum breakpoint

--- components/Container.jsx
+++ components/Container.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function Container({ children, className }) {
   return (
     <div
-      className={`relative z-10 px-6 sm:px-0 sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto py-16 ${className}`}
+      className={`relative z-10 px-6 sm:px-0 sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto py-6 lg:py-16 ${className}`}
     >
       {children}
     </div>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import classNames from "classnames";
-import { AnimatePresence } from "framer-motion";
 import { IconMoon, IconSun } from "./Icons";
 
 export default function Nav() {
@@ -9,15 +8,18 @@ export default function Nav() {
 
   return (
     <header className="w-full relative z-10 font-poppin">
-      <nav className="py-6 mx-auto max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl">
-        <div className="space-y-5 md:space-y-0 md:flex md:items-center md:justify-between">
+      <nav
+        className="py-6 px-6 sm:px-0 mx-auto sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl"
+        id="nav"
+      >
+        <div className="flex items-center justify-between">
           {/* brand */}
-          <div className="text-center md:text-left font-bold text-2xl text-gray-900 dark:text-white">
+          <div className="text-left font-bold text-xl lg:text-2xl text-gray-900 dark:text-white">
             Gun!
           </div>
           {/* links */}
-          <div className="text-center md:text-right space-x-10">
-            <Link href="/">
+          <div className="text-center lg:place-self-end space-x-8 lg:space-x-16">
+            <Link href="/" scroll={false}>
               <a
                 className={classNames(anchorClasses, {
                   [activeClasses]: router.pathname == "/",
@@ -26,7 +28,7 @@ export default function Nav() {
                 Tentang
               </a>
             </Link>
-            <Link href="/portfolio">
+            <Link href="/portfolio" scroll={false}>
               <a
                 className={classNames(anchorClasses, {
                   [activeClasses]: router.pathname == "/portfolio",
@@ -35,11 +37,9 @@ export default function Nav() {
                 Portfolio
               </a>
             </Link>
-            <AnimatePresence exitBeforeEnter>
-              <IconMoon key="1" />
-              <IconSun key="2" />
-            </AnimatePresence>
           </div>
+          <IconMoon key="1" />
+          <IconSun key="2" />
         </div>
       </nav>
     </header>
@@ -47,5 +47,5 @@ export default function Nav() {
 }
 
 const anchorClasses =
-  "inline-block text-lg text-gray-700 dark:text-gray-50 pb-1 hover:border-b-2 hover:border-gray-700 dark:hover:border-gray-200 transition duration-200 ease-out";
+  "inline-block text-base lg:text-lg text-gray-700 dark:text-gray-50 pb-1 hover:border-b-2 hover:border-gray-700 dark:hover:border-gray-200 transition duration-200 ease-out";
 const activeClasses = "border-b-2 border-gray-700 dark:border-gray-200";
