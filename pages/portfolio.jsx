2631442a0990a521a650495d39f3d67a6baf28ec
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-21 12:49:23 +0800

    move profile link section to findoutmore.jsx

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -7,18 +7,16 @@ import PortfolioImage from "../components/PortfolioImage";
 import PortfolioSection from "../components/PortfolioSection";
 import PortfolioTitle from "../components/PortfolioTitle";
 import PortfolioWrapper from "../components/PortfolioWrapper";
+import FindOutMore from "../components/FindOutMore";
 import GradientBg2 from "../components/GradientBg2";
 import GradientBg3 from "../components/GradientBg3";
 import {
   IconBootstrap,
   IconCSS,
-  IconDribbble,
   IconFigma,
   IconFirebase,
-  IconGithub,
   IconHTML,
   IconJS,
-  IconLinkedIn,
   IconNodeJS,
   IconReact,
   IconTailwind,
@@ -142,35 +140,7 @@ export default function Portfolio() {
           </PortfolioSection>
         </Container>
 
-        <div className="relative z-10 px-6 lg:px-0 py-12 text-center">
-          <h3 className="text-xl md:text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-pink-600">
-            Ingin menciptakan sesuatu yang memiliki visual yang indah?
-          </h3>
-          <div className="text-gray-400 mt-12 mb-5">Temukan saya di</div>
-          <div className="flex items-center justify-center space-x-5 lg:space-x-10 my-5">
-            <a
-              href="https://github.com/mrizkigunawan"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconGithub />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rizki-gunawan-609a29150/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconLinkedIn />
-            </a>
-            <a
-              href="https://dribbble.com/rizkiguna"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconDribbble />
-            </a>
-          </div>
-        </div>
+        <FindOutMore />
         <GradientBg2 />
       </div>
       <GradientBg />
