3e7a1094794615f0900ad24c6fef808c1c5b3378
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-21 12:50:14 +0800

    add page transition animation and modifed meta description

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,27 @@
 import Head from "next/head";
 import Nav from "../components/Nav";
+import PageTransition from "../components/PageTransition";
 import { ThemeWrapper } from "../context/AppContext";
 import "../styles/style.css";
 
 // eslint-disable-next-line react/prop-types
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeWrapper>
       <Head>
         <title>Gun Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="keywords" content="gunawan" />
-        <meta name="description" content="rizki gunawan portfolio" />
+        <meta name="keywords" content="rizki gunawan portofolio" />
+        <meta
+          name="description"
+          content="Seorang Front-end Dev yang suka membangun tampilan website yang memiliki performa yang cepat, interaktifitas dan visual yang keren"
+        />
       </Head>
       <Nav />
-      <Component {...pageProps} />
+      {/* eslint-disable-next-line react/prop-types */}
+      <PageTransition route={router.route}>
+        <Component {...pageProps} />
+      </PageTransition>
     </ThemeWrapper>
   );
 }
