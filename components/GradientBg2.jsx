6f419d7d8c0ab26488d9142fdc10cda371903e74
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-24 18:09:38 +0800

    change img x position

--- components/GradientBg2.jsx
+++ components/GradientBg2.jsx
@@ -7,7 +7,7 @@ export default function GradientBg2() {
       <img
         src="/img/Eclipse-2.png"
         alt="gradient"
-        className="w-[700px] md:w-[1000px] h-[700px] md:h-[1000px] transform translate-x-60 md:translate-x-[500px] translate-y-1/3 opacity-50 blur -rotate-180"
+        className="w-[500px] h-[500px] md:w-[800px] md:h-[800px] transform translate-x-60 md:translate-x-[400px] translate-y-1/3 opacity-50 blur -rotate-180"
       />
     </GradientWrapper>
   );
