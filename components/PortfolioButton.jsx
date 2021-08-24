4c0d253a07a1f5ef27fc5e9341014997cf3dbad7
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-08-24 18:10:18 +0800

    change button color and roundness

--- components/PortfolioButton.jsx
+++ components/PortfolioButton.jsx
@@ -8,7 +8,7 @@ export default function PortfolioButton() {
       <Link href="/portfolio">
         <motion.a
           variants={childVariants}
-          className="px-7 py-3 inline-block bg-gray-800 dark:bg-gray-200 text-gray-50 dark:text-gray-800 shadow-xl hover:bg-gray-700 dark:hover:bg-gray-100 transition duration-200 ease-out cursor-pointer"
+          className="px-7 py-3 inline-block bg-gray-800 dark:bg-yellow-600 text-gray-50 dark:text-gray-50 shadow-xl hover:bg-gray-700 dark:hover:bg-gray-100 transition duration-200 ease-out cursor-pointer rounded"
         >
           Lihat Portofolio <span className="ml-2">&rarr;</span>
         </motion.a>
